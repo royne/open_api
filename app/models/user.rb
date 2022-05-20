323eb5c95a6b59b1a491553b3bdd12f07facce74
royne <royne.chaves@gmail.com>
2022-05-20 10:38:36 -0500

    add base db

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+  rolify
+  has_secure_password
+
+  belongs_to :course, optional: true
+
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z]+[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
+  validates :email, presence: true, length: { maximum: 255 },
+  format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password,  presence: true, length: { minimum: 6, maximum: 50 }, allow_nil: true
+
+  private
+
+    def downcase_email
+      self.email.downcase!
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      
+      resources :users
+      resources :courses
+      resources :books
 
     end
   end 
 end
-  
